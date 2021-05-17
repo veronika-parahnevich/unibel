81e83469e04e00635cbfd09fbb973e2acdbb89cf
Моисеев Андрей Владимирович <moiseev.andrei@otr.ru>
2021-05-17 12:42:25 +0300

    Init project

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(window).scroll(function () {
+    progressbar()
+});
+
+function progressbar() {
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
+    document.getElementById("progress-bar").style.width = scrolled + "%";
+}
